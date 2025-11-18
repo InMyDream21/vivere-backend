d893f699e6abc20469b4409479ebcedc54c281dd
Hikaru <ahmed.nizhan@gmail.com>
2025-11-18 15:49:22 +0700

    feat: update prompt

--- app/gemini.py
+++ app/gemini.py
@@ -25,10 +25,11 @@ def generate_suggestions_for_image(image_bytes: bytes, content_type: str) -> str
         contents=[
             image_content,
             """
-            Buat 1 pertanyaan pembuka singkat (hanya 1 kalimat) yang ramah dan mudah dipahami untuk memulai percakapan dengan orang dengan demensia (OdD) berdasarkan gambar ini. 
-            Gunakan Bahasa Indonesia yang sopan, empatik, kalem, dan mudah dipahami. 
-            Hindari menguji ingatan atau topik medis. 
-            Fokus pada hal-hal positif, kenangan bahagia, atau pengalaman sehari-hari yang sederhana. 
+            Buat 1 pertanyaan singkat (hanya 1 kalimat berisi maksimum 8 kata per kalimat) yang jelas dan mudah dipahami untuk memulai percakapan dengan orang dengan demensia (OdD) berdasarkan gambar ini. 
+            Gunakan Bahasa Indonesia yang santai, tidak berkonotasi negatif, jelas, singkat, dan mudah dipahami. 
+            Hindari menanyakan hal-hal yang sangat rumit, 
+            Fokus pada hal-hal positif tentang kegiatan yang ada di gambar, kenangan bahagia di gambar, atau pengalaman sehari-hari yang sederhana. 
+            Hindari pertanyaan yang terlalu umum. 
             Format keluaran hanya dalam JSON: {\"question\": ... hanya 1 item ...}
             """
         ]
